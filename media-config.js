b449cf2e18380258f70fbdc01372a7cd78d01a28
wasjaoon-cyber <wasjaoon@gmail.com>
2026-09-07 20:20:58 +0700

    Update media-config.js

--- media-config.js
+++ media-config.js
@@ -1,131 +1,19 @@
-/* EDIT FILE INI UNTUK MEMASUKKAN MEDIA. Jangan hapus tanda kutip. */
-window.MEDIA_CONFIG = {
-  "sejarah-kendaraan.html": {
-    "images": [],
-    "youtube": "",
-    "video": "",
-    "animation": "",
-    "googleForm": "",
-    "wayground": ""
-  },
-  "revolusi-industri.html": {
-    "images": [],
-    "youtube": "",
-    "video": "",
-    "animation": "",
-    "googleForm": "",
-    "wayground": ""
-  },
-  "motor-bakar.html": {
-    "images": [],
-    "youtube": "",
-    "video": "",
-    "animation": "",
-    "googleForm": "",
-    "wayground": ""
-  },
-  "teknologi-konvensional.html": {
-    "images": [],
-    "youtube": "",
-    "video": "",
-    "animation": "",
-    "googleForm": "",
-    "wayground": ""
-  },
-  "teknologi-terkini.html": {
-    "images": [],
-    "youtube": "",
-    "video": "",
-    "animation": "",
-    "googleForm": "",
-    "wayground": ""
-  },
-  "kelas-xi-pengapian.html": {
-    "images": [],
-    "youtube": "",
-    "video": "",
-    "animation": "",
-    "googleForm": "",
-    "wayground": ""
-  },
+const MEDIA_CONFIG = {
   "kelas-xi-pengisian.html": {
-    "images": [],
-    "youtube": "",
-    "video": "",
-    "animation": "",
-    "googleForm": "",
-    "wayground": ""
-  },
-  "kelas-xi-starter.html": {
-    "images": [],
-    "youtube": "",
-    "video": "",
-    "animation": "",
-    "googleForm": "",
-    "wayground": ""
-  },
-  "kelas-xi-penerangan-tanda.html": {
-    "images": [],
-    "youtube": "",
-    "video": "",
-    "animation": "",
-    "googleForm": "",
-    "wayground": ""
-  },
-  "kelas-xi-instrumen-sinyal.html": {
-    "images": [],
-    "youtube": "",
-    "video": "",
-    "animation": "",
-    "googleForm": "",
-    "wayground": ""
-  },
-  "kelas-xi-remote-control.html": {
-    "images": [],
-    "youtube": "",
-    "video": "",
-    "animation": "",
-    "googleForm": "",
-    "wayground": ""
-  },
-  "kelas-xii-motor-listrik-hybrid.html": {
-    "images": [],
-    "youtube": "",
-    "video": "",
-    "animation": "",
-    "googleForm": "",
-    "wayground": ""
-  },
-  "kelas-xii-engine-management-system.html": {
-    "images": [],
-    "youtube": "",
-    "video": "",
-    "animation": "",
-    "googleForm": "",
-    "wayground": ""
-  },
-  "kelas-xii-pengelolaan-bengkel.html": {
-    "images": [],
-    "youtube": "",
-    "video": "",
-    "animation": "",
-    "googleForm": "",
-    "wayground": ""
-  },
-  "kelas-xii-praktik-kerja-industri.html": {
-    "images": [],
-    "youtube": "",
-    "video": "",
-    "animation": "",
-    "googleForm": "",
-    "wayground": ""
-  },
-  "kelas-xii-uji-kompetensi-keahlian.html": {
-    "images": [],
-    "youtube": "",
-    "video": "",
-    "animation": "",
-    "googleForm": "",
-    "wayground": ""
+    images: [
+      "media/rangkaian_pengisian.jpg"
+    ],
+
+    youtube: "",
+
+    video:
+      "https://raw.githubusercontent.com/wasjaoon-cyber/media-pembelajaran-interaktif/main/sistem-pengisian.mp4",
+
+    animation:
+      "https://raw.githubusercontent.com/wasjaoon-cyber/media-pembelajaran-interaktif/main/analogi-sistem-pengisian.mp4",
+
+    googleForm: "",
+
+    wayground: ""
   }
 };
